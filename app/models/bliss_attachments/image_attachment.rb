8272de6fdac88c66acf617248cb90746b3652d30
Simon Kiener <jugglinghobo@gmail.com>
2014-05-02 15:21:33 +0200

    changed callback to after_post_process

--- app/models/bliss_attachments/image_attachment.rb
+++ app/models/bliss_attachments/image_attachment.rb
@@ -1,7 +1,7 @@
 module BlissAttachments
-  class ImageAttachment < Attachment  
-    before_save :overwrite_content_type_file_name
-    
+  class ImageAttachment < Attachment
+    after_post_process :overwrite_content_type_file_name
+
     has_attached_file :file,
                       :styles      => { :original => {:geometry => "100%", :format => "png"},
                                         :large => {:geometry => "500x500", :format => "png"},
@@ -9,20 +9,18 @@ module BlissAttachments
                                         :thumb => {:geometry => "75x100", :format => "png"}
                       },
                       :default_url => "/system/missing_thumb.png"
-                      
+
     validates_attachment_content_type :file, :content_type => ["image/jpg", "image/jpeg", "image/tiff", "image/png"]
-    
+
     private
-    
+
     def overwrite_content_type_file_name
-      unless self.persisted?
-        new_file_name = (self.file_file_name.split /\./).first
-        new_file_name += ".png"
-        self.file_file_name = new_file_name
-        self.file_content_type = "image/png"
-      end
+      new_file_name = (self.file_file_name.split /\./).first
+      new_file_name += ".png"
+      self.file_file_name = new_file_name
+      self.file_content_type = "image/png"
     end
-    
-             
+
+
   end
 end
