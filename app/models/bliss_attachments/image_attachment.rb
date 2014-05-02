a77529b2f9ab6c860757aa5fa2514d768b111166
simplay <michael.single@students.unibe.ch>
2014-05-02 14:00:09 +0200

    ImageAttachment attachments are always converted to png files
    implemented corresponding updater before_save hook-in.

--- app/models/bliss_attachments/image_attachment.rb
+++ app/models/bliss_attachments/image_attachment.rb
@@ -1,5 +1,7 @@
 module BlissAttachments
   class ImageAttachment < Attachment  
+    before_save :overwrite_content_type_file_name
+    
     has_attached_file :file,
                       :styles      => { :original => {:geometry => "100%", :format => "png"},
                                         :large => {:geometry => "500x500", :format => "png"},
@@ -8,6 +10,17 @@ module BlissAttachments
                       },
                       :default_url => "/system/missing_thumb.png"
                       
-    validates_attachment_content_type :file, :content_type => ["image/jpg", "image/jpeg", "image/tiff", "image/png"]         
+    validates_attachment_content_type :file, :content_type => ["image/jpg", "image/jpeg", "image/tiff", "image/png"]
+    
+    private
+    
+    def overwrite_content_type_file_name
+      new_file_name = (self.file_file_name.split /\./).first
+      new_file_name += ".png"
+      self.file_file_name = new_file_name
+      self.file_content_type = "image/png"
+    end
+    
+             
   end
 end
