4800ecbd9327d2a664f1d2ddf08b26629e39eca8
Simon Kiener <jugglinghobo@gmail.com>
2023-01-19 14:54:31 +0100

    removed attr_accessible from Attachment model

--- app/models/bliss_attachments/attachment.rb
+++ app/models/bliss_attachments/attachment.rb
@@ -1,7 +1,5 @@
 module BlissAttachments
   class Attachment < ActiveRecord::Base
-    attr_accessible :parent_type, :parent_id, :file, :file_file_name,
-      :file_content_type, :file_file_size, :type, :favourited
 
     belongs_to :parent, :polymorphic => true
     has_attached_file :file,
